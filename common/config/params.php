46f49678692622d88978b197eecb82c42cd04645
john ramirez <jonnywells2020@gmail.com>
2025-07-13 07:45:59 -0400

    appName param changed

--- common/config/params.php
+++ common/config/params.php
@@ -21,5 +21,5 @@ $config = require __DIR__ . '/main-local.php';
 return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
-    'appName' => "Combo manager",
+    'appName' => "Modulo Ventas Iphone",
 ];
